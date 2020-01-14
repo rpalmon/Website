ea0fc03218dab6579de5e7d11faf4dbec91eca9d
--replace-all <--replace-all>
2020-01-14 15:56:03 +0200

    Updates

---
+++ precache-manifest.f7bb9afbc89c049adb07900652b272fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0008986b950112709e67c1f01e9f6df",
+    "url": "/Website/index.html"
+  },
+  {
+    "revision": "f5b16ec6269826049e2e",
+    "url": "/Website/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7dc7a2272f689031a8bd",
+    "url": "/Website/static/js/2.8a565ee4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Website/static/js/2.8a565ee4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5b16ec6269826049e2e",
+    "url": "/Website/static/js/main.010d9fe7.chunk.js"
+  },
+  {
+    "revision": "2a80963c30fa2e6f5aad",
+    "url": "/Website/static/js/runtime-main.2da07f36.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Website/static/media/logo.5d5d9eef.svg"
+  }
+]);
