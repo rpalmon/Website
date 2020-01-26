89f52eb2bd6d30e48f204b3f534eb340f43a6740
--replace-all <--replace-all>
2020-01-26 19:08:50 +0200

    js

--- WebDirect/main.js
+++ WebDirect/main.js
@@ -1,11 +1,26 @@
-var urls = [
-    'https://www.google.com'
-]
+const urlHTML = document.getElementById('urls');
+let urls = [];
+
+if(localStorage.getItem('urls') === null){
+    localStorage.setItem('urls', urls);
+}else{
+    urls = JSON.parse(localStorage.getItem('urls'));
+}
 // if(localStorage.getItem(''))
 
+for(let i = 0; i < urls.length; i++){
+    console.log(urls[i]);
+    urlHTML.innerHTML += `<div>` + urlTemplate(urls[i]) + `</div>`;
+}
+
+function urlTemplate(url){ 
+    return (`<div class='urls'>` + url + `</div>`)
+}
 function addURL(e){
     e.preventDefault();
     console.log(e.target.url.value);
     urls.push(e.target.url.value);
-
+    localStorage.setItem('urls', JSON.stringify(urls));
+    urlHTML.innerHTML += urlTemplate(urls[urls.length-1]);
+    
 }
