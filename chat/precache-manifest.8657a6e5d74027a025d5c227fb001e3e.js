93725aefc4dd75db521a0a56bdc6b4274e4bd278
Roee Palmon <new.roeepalmon@gmail.com>
2020-03-28 14:16:17 +0300

    added chat

---
+++ chat/precache-manifest.8657a6e5d74027a025d5c227fb001e3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "656a52ee3f6b8b5e9e7e25a6847f006d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76951eaa000d9139dc49",
+    "url": "/static/css/main.6cd6a5e9.chunk.css"
+  },
+  {
+    "revision": "c420a9dfc80f7116949f",
+    "url": "/static/js/2.8df8f370.chunk.js"
+  },
+  {
+    "revision": "0d5fd661578b0945bc2d83c75d64905a",
+    "url": "/static/js/2.8df8f370.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76951eaa000d9139dc49",
+    "url": "/static/js/main.8f4893e3.chunk.js"
+  },
+  {
+    "revision": "f7758ab2620046454ad0",
+    "url": "/static/js/runtime-main.0e64ef1c.js"
+  }
+]);
