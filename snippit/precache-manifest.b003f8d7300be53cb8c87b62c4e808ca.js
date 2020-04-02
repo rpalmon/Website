290283e6002741bde1300050b6d66e30157af286
Roee Palmon <new.roeepalmon@gmail.com>
2020-04-03 00:37:01 +0300

    Add files via upload

---
+++ snippit/precache-manifest.b003f8d7300be53cb8c87b62c4e808ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ca11db4cbfec115892b1c3cffef41c7",
+    "url": "/snippit/index.html"
+  },
+  {
+    "revision": "2df037292cd971aa8fae",
+    "url": "/snippit/static/css/main.40a725de.chunk.css"
+  },
+  {
+    "revision": "ecc96538c47a24257c15",
+    "url": "/snippit/static/js/2.d6840e77.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/snippit/static/js/2.d6840e77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2df037292cd971aa8fae",
+    "url": "/snippit/static/js/main.935b58cc.chunk.js"
+  },
+  {
+    "revision": "85cf4427278143427250",
+    "url": "/snippit/static/js/runtime-main.e81dccd0.js"
+  }
+]);
--- snippit/service-worker.js
+++ snippit/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b3a8f5eb8e6354accc75bf5cfceb420e.js"
+  "/snippit/precache-manifest.b003f8d7300be53cb8c87b62c4e808ca.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/snippit/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
