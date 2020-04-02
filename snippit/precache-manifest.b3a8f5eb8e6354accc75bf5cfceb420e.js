4af9e820b490d789b78d6145db25f9588bf13490
Roee Palmon <new.roeepalmon@gmail.com>
2020-04-03 00:13:58 +0300

    Add files via upload

---
+++ snippit/precache-manifest.b3a8f5eb8e6354accc75bf5cfceb420e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fde9999d745c1fb9ce1c2b4f269f02e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0829b435b87c0e433429",
+    "url": "/static/css/main.40a725de.chunk.css"
+  },
+  {
+    "revision": "ecc96538c47a24257c15",
+    "url": "/static/js/2.d6840e77.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.d6840e77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0829b435b87c0e433429",
+    "url": "/static/js/main.f517304e.chunk.js"
+  },
+  {
+    "revision": "97b6cdb9e189e26f0c0a",
+    "url": "/static/js/runtime-main.842d89fb.js"
+  }
+]);
